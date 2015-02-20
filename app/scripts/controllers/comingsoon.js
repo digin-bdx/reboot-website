bbf9427795085520423d7c8a3f94345ab33cc89a
bltnico <bellot.n@gmail.com>
2015-02-20 16:02:07 +0100

    Fix container padding

--- app/scripts/controllers/comingsoon.js
+++ app/scripts/controllers/comingsoon.js
@@ -11,8 +11,7 @@ angular.module('digin')
     .controller('ComingSoonCtrl', function ($scope, $http, $document) {
 
         $scope.consummers = {};
-        $scope.scrolled = true;
-
+        
         $http.get('dummy/consummers.json').success(function(data){
             $scope.consummers = data.consummers;
             console.log($scope.consummers);
